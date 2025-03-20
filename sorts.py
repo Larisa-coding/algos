17dccc64f7f0cc23a596cd83f696e02014a4fcb6
Larisa-coding <laisa.dubinina.92@gmail.com>
2025-03-20 18:57:06 +0800

    added HW code sorting

---
+++ sorts.py
@@ -0,0 +1,48 @@
+# пузырьковая
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+# быстрая
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
+
+# выбором
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
+
+# вставками
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
+# Примеры использования
+if __name__ == "__main__":
+    test_array = [64, 34, 25, 12, 22, 11, 90]
+    print("Bubble Sort:", bubble_sort(test_array[:]))
+    print("Quick Sort:", quick_sort(test_array[:]))
+    print("Selection Sort:", selection_sort(test_array[:]))
+    print("Insertion Sort:", insertion_sort(test_array[:]))
